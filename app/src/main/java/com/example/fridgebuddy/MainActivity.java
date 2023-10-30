8bf8157a0eafac06ec7eb791a42ca4da2aefebd5
zlain <zachartlain@gmail.com>
2023-10-30 16:34:19 -0400

    Moved scan function to a utility file to try to fix issues with database. See if I can fix the issues by moving out of MainActivity. May be causing issues trying to insert to db from there?

--- app/src/main/java/com/example/fridgebuddy/MainActivity.java
+++ app/src/main/java/com/example/fridgebuddy/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.fridgebuddy;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.example.fridgebuddy.util.Util;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,32 +14,26 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.fridgebuddy.databinding.ActivityMainBinding;
 
-// imports for scanning -ZL
-import com.google.mlkit.common.MlKitException;
-import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
-import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
-import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button scanMove;
     private ActivityMainBinding binding;
 
-    // handler for user fridge
-    private DBHandler dbHandler;
+    private Util util;
+    private AppDatabase database;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // creating dbhandler class for user fridge and giving it context
-        dbHandler = new DBHandler(MainActivity.this);
+        // initialize util method and database
+        util = new Util();
+        database = AppDatabase.getDatabase(getApplicationContext());
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -55,60 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         scanMove=findViewById(R.id.scannow);
         scanMove.setOnClickListener(v -> {
-            Scan();
+            util.Scan(MainActivity.this, database);
         });
-    }
-
-    // function to do the scanning -ZL
-    public void Scan() {
-        // create a new instance of the options and barcode scanner and build it, can use this to change
-        // options in the future if we want or change the context that the barcode is running in
-        GmsBarcodeScannerOptions.Builder optionsBuilder = new GmsBarcodeScannerOptions.Builder();
-        GmsBarcodeScanner gmsBarcodeScanner =
-                GmsBarcodeScanning.getClient(getApplicationContext(), optionsBuilder.build());
-
-        // start the barcode scanning and do something on success/fail/cancel
-        // these will be changed in the future to add items and display error codes to the user
-        gmsBarcodeScanner
-                .startScan()
-                .addOnSuccessListener(this::scanSuccessful)
-                .addOnFailureListener(
-                        this::getErrorMessage)
-                .addOnCanceledListener(
-                        () -> Toast.makeText(MainActivity.this, getString(R.string.error_scanner_cancelled), Toast.LENGTH_SHORT).show());
-    }
-
-    // function for a successful barcode reading -ZL
-    private void scanSuccessful(com.google.mlkit.vision.barcode.common.Barcode barcode) {
-        // just display something in the home page currently for debug
-        // will incorporate with the database in future change
-        String barcodeValue =
-                String.format(
-                        Locale.US,
-                        "Barcode Value: %s",
-                        barcode.getDisplayValue());
-
-        dbHandler.addItem(barcodeValue);
-
-        Toast.makeText(MainActivity.this, "Item with UPC of " + barcodeValue + " has been added.", Toast.LENGTH_SHORT).show();
-    }
-
-    // function if an exception is thrown while trying to read barcodes -ZL
-    @SuppressLint("SwitchIntDef")
-    private void getErrorMessage(Exception e) {
-        if (e instanceof MlKitException) {
-            switch (((MlKitException) e).getErrorCode()) {
-                case MlKitException.PERMISSION_DENIED:
-                    Toast.makeText(MainActivity.this, getString(R.string.error_camera_permission_not_granted), Toast.LENGTH_SHORT).show();
-                    return;
-                case MlKitException.UNAVAILABLE:
-                    Toast.makeText(MainActivity.this, getString(R.string.error_app_name_unavailable), Toast.LENGTH_SHORT).show();
-                    return;
-                default:
-                    Toast.makeText(MainActivity.this, getString(R.string.error_default_message, e), Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
     }
 }
---
+++ app/src/main/java/com/example/fridgebuddy/util/Util.java
@@ -0,0 +1,98 @@
+package com.example.fridgebuddy.util;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
+import android.widget.Toast;
+
+
+import com.example.fridgebuddy.AppDatabase;
+import com.example.fridgebuddy.Item;
+import com.example.fridgebuddy.R;
+import com.google.mlkit.common.MlKitException;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions;
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
+
+import java.util.Locale;
+
+
+/**
+ * Class to hold utilities used throughout the app.
+ */
+public class Util extends Application {
+    private Activity activity;
+    // db test
+    private AppDatabase database;
+
+    /**
+     * Scan function, allows us to use the barcode scanner in whatever context we are in
+     * by using the activity parameter
+     * @param activity
+     */
+
+    public void Scan(Activity activity, AppDatabase database) {
+        this.activity = activity;
+        this.database = database;
+
+
+        // create a new instance of the options and barcode scanner and build it, can use this to change
+        // options in the future if we want or change the context that the barcode is running in
+        GmsBarcodeScannerOptions.Builder optionsBuilder = new GmsBarcodeScannerOptions.Builder();
+        GmsBarcodeScanner gmsBarcodeScanner =
+                GmsBarcodeScanning.getClient(activity.getApplicationContext(), optionsBuilder.build());
+
+        // start the barcode scanning and do something on success/fail/cancel
+        // these will be changed in the future to add items and display error codes to the user
+        gmsBarcodeScanner
+                .startScan()
+                .addOnSuccessListener(
+                        this::scanSuccessful)
+                .addOnFailureListener(
+                        this::getErrorMessage)
+                .addOnCanceledListener(
+                        () -> Toast.makeText(activity.getApplicationContext(), activity.getApplicationContext().getString(R.string.error_scanner_cancelled), Toast.LENGTH_SHORT).show());
+    }
+
+    // function for a successful barcode reading -ZL
+    private void scanSuccessful(com.google.mlkit.vision.barcode.common.Barcode barcode) {
+        // just display something in the home page currently for debug
+        // will incorporate with the database in future change
+        String barcodeValue =
+                String.format(
+                        Locale.US,
+                        "Barcode Value: %s",
+                        barcode.getDisplayValue());
+
+
+        Item testItem = new Item(barcodeValue, "test", "10/30/2023");
+
+        database.itemDao().upsertItem(testItem);
+
+
+        Toast.makeText(activity.getApplicationContext(), "Item with UPC of " + barcodeValue + " has been added.", Toast.LENGTH_SHORT).show();
+    }
+
+    // function if an exception is thrown while trying to read barcodes -ZL
+    @SuppressLint("SwitchIntDef")
+    private void getErrorMessage(Exception e) {
+        if (e instanceof MlKitException) {
+            switch (((MlKitException) e).getErrorCode()) {
+                case MlKitException.PERMISSION_DENIED:
+                    Toast.makeText(activity.getApplicationContext(), activity.getApplicationContext().getString(R.string.error_camera_permission_not_granted), Toast.LENGTH_SHORT).show();
+                    return;
+                case MlKitException.UNAVAILABLE:
+                    Toast.makeText(activity.getApplicationContext(), activity.getApplicationContext().getString(R.string.error_app_name_unavailable), Toast.LENGTH_SHORT).show();
+                    return;
+                default:
+                    Toast.makeText(activity.getApplicationContext(), activity.getApplicationContext().getString(R.string.error_default_message, e), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(activity.getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+}
