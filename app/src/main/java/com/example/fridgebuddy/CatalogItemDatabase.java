51b822b4f13d1f3336750ff047d8d72e954425e9
zlain <zachartlain@gmail.com>
2023-11-10 17:59:29 -0500

    changed context to context.getApplicationContext() changed version

--- app/src/main/java/com/example/fridgebuddy/CatalogItemDatabase.java
+++ app/src/main/java/com/example/fridgebuddy/CatalogItemDatabase.java
@@ -6,14 +6,14 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {CatalogItem.class}, version = 1)
+@Database(entities = {CatalogItem.class}, version = 2)
 public abstract class CatalogItemDatabase extends RoomDatabase {
     private static CatalogItemDatabase INSTANCE;
     public abstract CatalogItemDao catalogItemDao();
 
     public static CatalogItemDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(context, CatalogItemDatabase.class, "catalog_items").createFromAsset("catalog_items.db").build();
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), CatalogItemDatabase.class, "catalog_items").createFromAsset("catalog_items.db").build();
         }
 
         return INSTANCE;
