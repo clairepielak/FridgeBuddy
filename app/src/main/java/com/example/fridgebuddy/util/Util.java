31a66f42e1b9d02629b53db2677e5bc18d7f5217
zlain <zachartlain@gmail.com>
2023-11-10 18:05:28 -0500

    Added CatalogDatabase Added Calendar and Date functions Added itemDB and catalogDB to functions Spent way too long trying to figure out why I couldnt get catalogItem to not be null. It was the String being formatted with "Barcode Value: " at the start... Fixed that Added functionality of getting Item from CatalogItemDatabase using scanner.

--- app/src/main/java/com/example/fridgebuddy/util/Util.java
+++ app/src/main/java/com/example/fridgebuddy/util/Util.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.app.Application;
 import android.widget.Toast;
 
+import com.example.fridgebuddy.CatalogItem;
+import com.example.fridgebuddy.CatalogItemDatabase;
 import com.example.fridgebuddy.ItemDatabase;
 import com.example.fridgebuddy.Item;
 import com.example.fridgebuddy.R;
@@ -15,8 +17,8 @@ import com.google.mlkit.vision.codescanner.GmsBarcodeScanning;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -27,14 +29,24 @@ import java.util.concurrent.Executors;
  */
 public class Util extends Application {
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private Item scannedItem;
+
+
+
+    // used to set exp dates
+    private final Calendar calendar = Calendar.getInstance();
+    private Date expirationDate = calendar.getTime();
+    // private SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyy");
+
 
     // Changed so the scan method can accept an instance of any activity -SM
     /**
      * Scan function, allows us to use the barcode scanner in whatever activity we want
      * @param activity give the current activity the scan is associated with
-     * @param database give the database that you want to access, in our case it will almost always be the AppDatabase
+     * @param itemDB give the database that you want to access to the ItemDatabase for adding items to users storage
+     * @param catalogDB allows us to read the data from the CatalogItemDatabase
      */
-    public void Scan(Activity activity, ItemDatabase database) {
+    public void Scan(Activity activity, ItemDatabase itemDB, CatalogItemDatabase catalogDB) {
         /*
           create a new instance of the options and barcode scanner and build it, can use this to change
           options in the future if we want or change the context that the barcode is running in
@@ -49,7 +61,7 @@ public class Util extends Application {
         */
         gmsBarcodeScanner
                 .startScan()
-                .addOnSuccessListener(barcode -> scanSuccessful(barcode, database, activity))
+                .addOnSuccessListener(barcode -> scanSuccessful(barcode, itemDB, catalogDB, activity))
                 .addOnFailureListener(e -> getErrorMessage(e, activity))
                 .addOnCanceledListener(() -> Toast.makeText(activity.getApplicationContext(), activity.getApplicationContext().getString(R.string.error_scanner_cancelled), Toast.LENGTH_SHORT).show());
     }
@@ -57,28 +69,35 @@ public class Util extends Application {
     /**
      * If a barcode scan is successful,
      * adds the item associated with that barcode to the user's fridge database.
-     * Will only add the item if it exists in our database of items we accounted for.
+     * Will only add the item if it exists in our catalog_items.db
      */
-    private void scanSuccessful(com.google.mlkit.vision.barcode.common.Barcode barcode, ItemDatabase database, Activity activity) {
+    private void scanSuccessful(com.google.mlkit.vision.barcode.common.Barcode barcode, ItemDatabase itemDB, CatalogItemDatabase catalogDB, Activity activity) {
         // just display something in the home page currently for debug
         // will incorporate with the database in future change
-        String barcodeValue =
-                String.format(
-                        Locale.US,
-                        "Barcode Value: %s",
-                        barcode.getDisplayValue());
+        String barcodeValue = String.format(barcode.getDisplayValue());
 
-
-        // test item
-        // Item testItem = new Item(barcodeValue, "test", "10/30/2023");
-
-
+        // get data on diff thread than main, may lock up ui
         executor.execute(() -> {
-            // this is where item will be upserted
-            // database.itemDao().upsertItem(testItem);
-        });
+            CatalogItem catalogItem = catalogDB.catalogItemDao().getCatalogItemByUPC(barcodeValue);
 
-        Toast.makeText(activity.getApplicationContext(), "Item with UPC of " + barcodeValue + " has been added.", Toast.LENGTH_SHORT).show();
+            if (catalogItem != null) {
+                // set expiration date
+                calendar.add(Calendar.DAY_OF_MONTH, catalogItem.getDaysUntilExp());
+                expirationDate = calendar.getTime();
+
+                // test = new Item("199901294", "Test",
+                scannedItem = new Item(barcodeValue, catalogItem.getName(), expirationDate);
+
+
+                itemDB.itemDao().upsertItem(scannedItem);
+
+
+                // Post a Runnable to the UI thread to display the Toast message
+                activity.runOnUiThread(() -> Toast.makeText(activity.getApplicationContext(), scannedItem.getName() + " with UPC of " + barcodeValue + " has been added.", Toast.LENGTH_LONG).show());
+            } else {
+                activity.runOnUiThread(() -> Toast.makeText(activity.getApplicationContext(), "Unable to scan.", Toast.LENGTH_LONG).show());
+            }
+        });
     }
 
     /**
@@ -105,24 +124,28 @@ public class Util extends Application {
 
     /**
      * Add an Item without using the barcode scanner,
-     * made for items that we don't have in our database of items.
+     * made for items that we don't have in our catalog_items.db.
      * Allows users to create their own items that we didn't account for
      * @param activity passes activity to allow the Toast to be shown
-     * @param database give the database that you want to access, in our case it will almost always be the AppDatabase
+     * @param itemDB give the database that you want to access, in our case it will almost always be the AppDatabase
      * @param name name of item given
      * @param dateString date of expiration, needs to be a String datatype, will be parsed to a Date
      */
-    public void AddItem(Activity activity, ItemDatabase database, String name, String dateString) {
+    public void AddItem(Activity activity, ItemDatabase itemDB, String name, String dateString) {
         try {
             // parse dateString to date
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
             Date date = dateFormat.parse(dateString);
 
-            // add to database
-            executor.execute(() -> database.itemDao().upsertItem(new Item(null, name, date)));
+            Item item = new Item(null, name, date);
 
-            //Lets the user know the item was added to their inventory
-            Toast.makeText(activity, "Item added successfully", Toast.LENGTH_SHORT).show();
+            // add to database
+            executor.execute(() -> {
+                itemDB.itemDao().upsertItem(item);
+
+
+                activity.runOnUiThread(() -> Toast.makeText(activity.getApplicationContext(), item.getName()  + " has been added.", Toast.LENGTH_LONG).show());
+            });
         } catch (ParseException e) {
             // Handle the parsing exception if the date format is incorrect.
 
