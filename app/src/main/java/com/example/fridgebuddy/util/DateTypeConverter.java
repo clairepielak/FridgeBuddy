cb7257ff8b61ab48ab305a5bd423eb78ce37ae5a
zlain <zachartlain@gmail.com>
2023-11-11 16:27:12 -0500

    added dateToString

--- app/src/main/java/com/example/fridgebuddy/util/DateTypeConverter.java
+++ app/src/main/java/com/example/fridgebuddy/util/DateTypeConverter.java
@@ -1,6 +1,10 @@
 package com.example.fridgebuddy.util;
 
+import android.annotation.SuppressLint;
+
 import androidx.room.TypeConverter;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTypeConverter {
@@ -13,5 +17,12 @@ public class DateTypeConverter {
     @TypeConverter
     public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
+    }
+
+    @TypeConverter
+    public static String dateToString(Date date) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyy");
+        return sdf.format(date);
     }
 }
