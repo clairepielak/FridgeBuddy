c8affc1c28ca65a8decf327c641c57cb742a92bd
zlain <zachartlain@gmail.com>
2023-11-13 13:58:57 -0500

    Refactored from DateTypeConverter, this will now convert Dates and Bitmaps

--- app/src/main/java/com/example/fridgebuddy/util/Converters.java
+++ app/src/main/java/com/example/fridgebuddy/util/Converters.java
@@ -1,14 +1,17 @@
 package com.example.fridgebuddy.util;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import androidx.room.TypeConverter;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateTypeConverter {
-
+public class Converters {
+    // date converters
     @TypeConverter
     public static Date fromTimeStamp(Long value) {
         return value == null ? null : new Date(value);
@@ -24,5 +27,19 @@ public class DateTypeConverter {
         @SuppressLint("SimpleDateFormat")
         SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyy");
         return sdf.format(date);
+    }
+
+    // bitmap converters
+    @TypeConverter
+    public static byte[] fromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+        return stream.toByteArray();
+    }
+
+    @TypeConverter
+    public static Bitmap toBitmap(byte[] byteArray) {
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
     }
 }
