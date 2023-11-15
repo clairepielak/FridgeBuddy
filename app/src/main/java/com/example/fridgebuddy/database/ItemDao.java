a8e063caa3375232e66ccd13c61adda601f757a1
zlain <zachartlain@gmail.com>
2023-11-15 13:35:59 -0500

    Was having an issue with setting alarms because it uses the id to denote what alarm is set.
    I added a getItem() function that allows us to get the new Item from the itemId that is autogenerated. Then the new @Transaction allows us to upsert and return the items in a single function (This is the idea of transactions)

--- app/src/main/java/com/example/fridgebuddy/database/ItemDao.java
+++ app/src/main/java/com/example/fridgebuddy/database/ItemDao.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Upsert;
 
 import java.util.List;
@@ -12,7 +13,16 @@ import java.util.List;
 @Dao
 public interface ItemDao {
     @Upsert
-    void upsertItem(Item... item);
+    Long upsertItem(Item... item);
+
+    @Query("SELECT * FROM user_items WHERE id = :itemId")
+    Item getItem(long itemId);
+
+    @Transaction
+    default Item upsertAndGet(Item item) {
+        long itemId = upsertItem(item);
+        return getItem(itemId);
+    }
 
     @Delete
     void deleteItem(Item item);
